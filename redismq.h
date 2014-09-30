0bd040eba31f20e1792fe6a97c93b41f92f9650d
ancurabiez <ancurabiez@localhost.localdomain>
2014-09-30 10:43:34 +0700

    Add comment info

--- redismq.h
+++ redismq.h
@@ -15,9 +15,21 @@
 #define TCP_SOCKET 0
 #define UNIX_SOCKET 1
 
+
+/* using unix socket: port = 0
+ *                    flag = UNIX_SOCKET
+ *                    path_host = socket path
+ * if return null, err must be free
+ */
 redisContext* rmq_connect(const char *path_host, int port,
                                int timeout, char **err, int flag);
+
+/* return null if error or value not found
+ * return must be free
+ */
 char* rmq_get(redisContext **rctx, const char *key, int timeout);
+
+/* return 0 if error */
 int rmq_put(redisContext **rctx, const char *key, char *val, int val_len);
 
 #endif
