fdd96cf9ec5462d0b61e41620ad2008f876db85f
ancurabiez <l.wijayakto@yahoo.com>
2015-09-16 14:28:00 +0700

    changed

--- redismq.c
+++ redismq.c
@@ -17,62 +17,92 @@
 #include "redismq.h"
 
 
-char host__[64];
-int port__;
-int flag__;
-int once = 0;
 
-
-redisContext* rmq_connect(const char *path_host, int port, int timeout,
+rmqContext* rmq_connect(const char *path_host, int port, int timeout,
                            char **err, int flag)
 {
    struct timeval tv;
-   redisContext *rctx;
+   rmqContext *rContext;
+   int i;
 
    tv.tv_sec = timeout;
    tv.tv_usec = 0;
 
-   if (!once) {
-      strncpy(host__, path_host, strlen(path_host));
-      port__ = port;
-      flag__ = flag;
+   rContext = je_malloc(sizeof(struct rmqContext));
+   assert (rContext);
 
-      once = 1;
-   }
+   memset (rContext, 0, sizeof(struct rmqContext));
+
+   rContext->host = je_malloc(16);
+   assert (rContext->host);
+   memset(rContext->host, 0, 16);
+
+   memcpy(rContext->host, path_host, 15);
+
+   rContext->port = port;
+   rContext->flag = flag;
 
    if (flag == UNIX_SOCKET)
-      rctx = redisConnectUnixWithTimeout(path_host, tv);
+      rContext->rctx = redisConnectUnixWithTimeout(path_host, tv);
    else
-      rctx = redisConnectWithTimeout(path_host, port, tv);
+      rContext->rctx = redisConnectWithTimeout(path_host, port, tv);
 
-   if (!rctx || rctx->err) {
-      if (rctx) {
-         char b1[128];
+   if (!rContext->rctx || rContext->rctx->err) {
+       char b1[128];
 
-         snprintf(b1, 128, "Queue connection error: %s\n", rctx->errstr);
-         redisFree(rctx);
-         *err = strdup(b1);
-         assert(err);
+      if (rContext->rctx) {
+         snprintf(b1, 128, "Queue connection error: %s\n", rContext->rctx->errstr);
+         redisFree(rContext->rctx);
+
+         i = strlen(b1);
+
+         *err = je_malloc(i + 1);
+         assert(*err);
+         memset(*err, 0, i);
+         memcpy(*err, b1, i);
          
          return NULL;
       } else {
-         *err = strdup("Queue connection error: can't "
-                        "allocate redis context\n");
-         assert(err);
+         snprintf(b1, 128, "Queue connection error: can't "
+                           "allocate redis context\n");
+         i = strlen(b1);
+
+         *err = je_malloc(i + 1);
+         assert(*err);
+         memset(*err, 0, i);
+         memcpy(*err, b1, i);
          
          return NULL;
       }
    }
 
-   return rctx;
+   return rContext;
+}
+
+void rmq_free(rmqContext *rctx)
+{
+  redisFree(rctx->rctx);
+  je_free(rctx->host);
+  je_free(rctx);
 }
 
 /* reconnect */
-static void rmq_reconnect(redisContext **rctx)
+static void rmq_reconnect(rmqContext **rctx)
 {
-   redisContext *rctx__;
+   rmqContext *rctx__ = NULL;
    char *err;
+   char host__[16];
    int i = 0;
+   int port__;
+   int flag__;
+
+   memset (host__, 0, 16);
+
+   memcpy(host__, (*rctx)->host, strlen((*rctx)->host));
+   port__ = (*rctx)->port;
+   flag__ = (*rctx)->flag;
+
+   rmq_free(*rctx);
 
    // trying reconnect till 60 times
    while (1) {
@@ -83,128 +113,126 @@ static void rmq_reconnect(redisContext **rctx)
          if (i >= 60) //give up
             exit(1);
 
-         free(err);
+         je_free(err);
          sleep(1);
       }
       else
          break;
    }
 
-   redisFree(*rctx);
    *rctx = rctx__;
 }
 
-static redisReply* multi_command_put(redisContext **rctx, const char *key,
-                                     char *val, int val_len)
-{
-   redisReply *rep;
-
-   rep = redisCommand(*rctx, "MULTI");
-   freeReplyObject(rep);
-
-   // add put statistic (if not exists)
-   rep = redisCommand(*rctx, "SETNX redismq:%s:stat:put 0", key);
-   freeReplyObject(rep);
-
-   // put data to queue
-   rep = redisCommand(*rctx, "RPUSH redismq:%s %b", key, val, val_len);
-   freeReplyObject(rep);
-
-   // increment put stat
-   rep = redisCommand(*rctx, "INCR redismq:%s:stat:put", key);
-   freeReplyObject(rep);
-
-   rep = redisCommand(*rctx, "EXEC");
-
-   return rep;
-}
-
 /* return : NULL if error */
-char* rmq_get(redisContext **rctx, const char *key, int timeout)
+char* rmq_get(rmqContext **rctx, const char *key, int timeout)
 {
    redisReply *rep = NULL;
    char *res = NULL;
 
-   // add get statistic (if not exists)
-   rep = redisCommand(*rctx, "SETNX redismq:%s:stat:get 0", key);
-   if (!rep) {
-      rmq_reconnect(rctx);
-
-      //re-execute
-      rep = redisCommand(*rctx, "SETNX redismq:%s:stat:get 0", key);
-   }
-   freeReplyObject(rep);
-   
    if (timeout > 0)
-      rep = redisCommand(*rctx, "BLPOP redismq:%s %d", key, timeout);
+      rep = redisCommand((*rctx)->rctx, "BLPOP redismq:%s %d", key, timeout);
    else
-      rep = redisCommand(*rctx, "LPOP redismq:%s", key);
+      rep = redisCommand((*rctx)->rctx, "LPOP redismq:%s", key);
 
    if (!rep) {
      rmq_reconnect(rctx);
 
      // re-execute
       if (timeout > 0)
-         rep = redisCommand(*rctx, "BLPOP redismq:%s %d", key, timeout);
+         rep = redisCommand((*rctx)->rctx, "BLPOP redismq:%s %d", key, timeout);
       else
-         rep = redisCommand(*rctx, "LPOP redismq:%s", key);
+         rep = redisCommand((*rctx)->rctx, "LPOP redismq:%s", key);
    }
 
-   if ((rep->type == 1) && (rep->elements == 0)) {
+   if ((rep->type == REDIS_REPLY_STRING) && (rep->elements == 0)) {
       if (rep->str) {
          res = je_malloc(rep->len);
          assert(res);
 
          memcpy(res, rep->str, rep->len);
-         freeReplyObject(rep);
-
-         // increment put stat
-         rep = redisCommand(*rctx, "INCR redismq:%s:stat:get", key);
-         freeReplyObject(rep);
-      } else
-         freeReplyObject(rep);
+      }
    }
-   else if ((rep->type == 2) && (rep->elements == 2)) {
+   else if ((rep->type == REDIS_REPLY_ARRAY) && (rep->elements == 2)) {
       if (rep->element[1]->str) {
          res = je_malloc(rep->element[1]->len);
          assert (res);
 
          memcpy(res, rep->element[1]->str, rep->element[1]->len);
-         freeReplyObject(rep);
+      }
+   }
 
-         // increment put stat
-         rep = redisCommand(*rctx, "INCR redismq:%s:stat:get", key);
-         freeReplyObject(rep);
-      } else
-         freeReplyObject(rep);
-   } else
-      freeReplyObject(rep);
-
+   freeReplyObject(rep);
    return res;
 }
 
 /* return : 1 on success */
 /*          0 on error */
-int rmq_put(redisContext **rctx, const char *key, char *val, int val_len)
+int rmq_put(rmqContext **rctx, const char *key, char *val, int val_len)
 {
    redisReply *rep = NULL;
    int ret = 0;
    
-   rep = multi_command_put(rctx, key, val, val_len);
+   rep = redisCommand((*rctx)->rctx, "RPUSH redismq:%s %b", key, val, val_len);
    if (!rep) {
       rmq_reconnect(rctx);
 
       //re-execute
-      rep = multi_command_put(rctx, key, val, val_len);
+      rep = redisCommand((*rctx)->rctx, "RPUSH redismq:%s %b", key, val, val_len);
    }
 
-   if (rep->elements == 3) {
-      if (rep->element[2]->type == REDIS_REPLY_INTEGER)
-        ret = 1;
-   }
+   if (rep->type == REDIS_REPLY_INTEGER)
+     ret = 1;
 
    freeReplyObject(rep);
-
    return ret;
 }
 
+int rmq_setter(rmqContext **rctx, const char *key, int expired,
+               char *val, int val_len)
+{
+   redisReply *rep = NULL;
+   int ret = 0;
+
+   rep = redisCommand((*rctx)->rctx, "SETEX %s %d %b", key, expired,
+                      val, val_len);
+
+   if (!strcmp(rep->str, "OK"))
+      ret = 1;
+
+   freeReplyObject(rep);
+   return ret;
+}
+
+int rmq_del(rmqContext **rctx, const char *key)
+{
+   redisReply *rep = NULL;
+   int ret = 0;
+
+   rep = redisCommand((*rctx)->rctx, "DEL %s", key);
+
+   if (rep->integer > 0)
+      ret = 1;
+
+   freeReplyObject(rep);
+   return ret;
+}
+
+char* rmq_getter(rmqContext **rctx, const char *key)
+{
+   redisReply *rep = NULL;
+   char *res = NULL;
+
+   rep = redisCommand((*rctx)->rctx, "GET %s", key);
+
+   if (rep->len > 0) {
+     if (rep->str) {
+        res = je_malloc(rep->len);
+        assert(res);
+
+        memcpy(res, rep->str, rep->len);
+     }
+   }
+
+   freeReplyObject(rep);
+   return res;
+}
--- redismq.h
+++ redismq.h
@@ -16,9 +16,22 @@
 #define TCP_SOCKET 0
 #define UNIX_SOCKET 1
 
-redisContext* rmq_connect(const char *path_host, int port,
+typedef struct rmqContext {
+  redisContext *rctx;
+  char *host;
+  int port;
+  int flag;
+} rmqContext;
+
+rmqContext* rmq_connect(const char *path_host, int port,
                                int timeout, char **err, int flag);
-char* rmq_get(redisContext **rctx, const char *key, int timeout);
-int rmq_put(redisContext **rctx, const char *key, char *val, int val_len);
+char* rmq_get(rmqContext **rctx, const char *key, int timeout);
+int rmq_put(rmqContext **rctx, const char *key, char *val, int val_len);
+
+int rmq_setter(rmqContext **rctx, const char *key, int expired,
+               char *val, int val_len);
+int rmq_del(rmqContext **rctx, const char *key);
+char* rmq_getter(rmqContext **rctx, const char *key);
+void rmq_free(rmqContext *rctx);
 
 #endif
